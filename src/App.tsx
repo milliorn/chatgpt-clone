5900a719e055c77407c3e2d43133ed325619c9e4
Scott Milliorn <scottmilliorn@gmail.com>
2023-04-18 21:15:22 -0700

    Side bar (#6)
    * button new chat
    
    * side bar html
    
    * import font
    
    * body css
    
    * app side bar main css
    
    * css, sidebar, button, main
    
    * ul
    
    * history history li css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-// main app 
+// main app
 function App(): JSX.Element {
-
   return (
-    <div className="App">
-      
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Lorem</li>
+        </ul>
+        <nav>
+          <p>Created by Scott Milliorn</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
