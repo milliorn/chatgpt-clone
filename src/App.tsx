1160b533795d6c7d23d0c87b42966023f8ad66b9
milliorn <scottmilliorn@gmail.com>
2023-04-18 21:23:41 -0700

    section main

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,22 @@ function App(): JSX.Element {
           <p>Created by Scott Milliorn</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>ChatGPT-Clone</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input type="text" />
+            <div id="submit"> {">"}</div>
+          </div>
+        </div>
+        <p className="info">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit nobis
+          ipsa impedit nulla voluptatum dolorem, voluptatem molestias repellat
+          ea. Nobis distinctio consequuntur ab perspiciatis quod fuga minima
+          natus ea rem.
+        </p>
+      </section>
     </div>
   );
 }
