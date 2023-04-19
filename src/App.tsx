9144194c5429e38284307a05638df8260ac4cab9
milliorn <scottmilliorn@gmail.com>
2023-04-18 20:39:37 -0700

    button new chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-// main app 
+// main app
 function App(): JSX.Element {
-
   return (
     <div className="App">
-      
+      <section className="side-bar">
+        <button>+ New Chat</button>
+      </section>
+      <section className="main"></section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
