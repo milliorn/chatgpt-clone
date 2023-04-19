d7996c49d02ce940bdf0fe727f8c3bcf1e86c6b6
milliorn <scottmilliorn@gmail.com>
2023-04-18 20:40:30 -0700

    side bar html

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App(): JSX.Element {
     <div className="App">
       <section className="side-bar">
         <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Created by Scott Milliorn</p>
+        </nav>
       </section>
       <section className="main"></section>
     </div>
