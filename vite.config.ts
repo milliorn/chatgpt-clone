1b7f7512b58ea9feb219a86a61e0f6afa8ed0302
milliorn <scottmilliorn@gmai.com>
2023-04-23 21:54:10 -0700

    fixed docker backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000,
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
   },
 });
